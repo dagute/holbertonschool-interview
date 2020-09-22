afe1e07b0fecdb9d7c3afc338ff0530a066bc954
dagute <dagute28@gmail.com>
2020-09-22 17:57:27 +0000

    first commit

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,23 +5,18 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
     n_bytes = 0
-    if not data or len(data) == 0:
-        return True
-    for b in data:
-        m = 1 << 7
-        if not n_bytes:
-            while m & b:
-                n_bytes += 1
-                m >>= 1
-        b = bin(b).replace('0b', '').rjust(8, '0')
-        if n_bytes != 0:
+    for c in data:
+        if n_bytes == 0:
+            if (c >> 5) == 0b110:
+                n_bytes = 1
+            elif (c >> 4) == 0b1110:
+                n_bytes = 2
+            elif (c >> 3) == 0b11110:
+                n_bytes = 3
+            elif (c >> 7):
+                return False
+        else:
+            if (c >> 6) != 0b10:
+                return False
             n_bytes -= 1
-            if not b.startswith('10'):
-                return False
-            if not n_bytes:
-                continue
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-        elif b[0] == '1':
-                n_bytes = len(b.split('0')[0]) - 1
-    return True
+    return n_bytes == 0
