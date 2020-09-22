9416c64357d3faf94118f73a613d96f0e3684aa6
dagute <dagute28@gmail.com>
2020-09-22 17:49:17 +0000

    first commit

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,14 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
     n_bytes = 0
+    if not data or len(data) == 0:
+        return True
     for b in data:
+        m = 1 << 7
+        if not n_bytes:
+            while m & b:
+                n_bytes += 1
+                m >>= 1
         b = bin(b).replace('0b', '').rjust(8, '0')
         if n_bytes != 0:
             n_bytes -= 1
