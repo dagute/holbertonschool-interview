de7004657bd9d8f1511935c87ae6e3f746de847e
dagute <dagute28@gmail.com>
2020-09-22 16:53:13 +0000

    first commit

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    n_bytes = 0
+    for b in data:
+        b = bin(b).replace('0b', '').rjust(8, '0')
+        if n_bytes != 0:
+            n_bytes -= 1
+            if not b.startswith('10'):
+                return False
+        elif b[0] == '1':
+                n_bytes = len(b.split('0')[0]) - 1
+    return True
